e570d6fbc63171424e567cc79e81466aeebdd44c
Bruna <brunaporato@gmail.com>
2024-02-23 19:36:53 -0300

    add section cases

---
+++ src/pages/home/sections/Cases/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from "@/src/components/Link";
+import { SectionTitle } from "@/src/components/SectionTitle";
+import Image from "next/image";
+import { CaseContent, CaseDescription, CasesContainer } from "./styles";
+
+import MockupMaternite from "@/src/assets/Mockup.png"
+import { ArrowUpRight } from "@phosphor-icons/react";
+
+export function CasesSection() {
+  return (
+    <CasesContainer>
+      <Image src={MockupMaternite} alt="" width={400} />
+      <CaseContent>
+        <SectionTitle span="Nosso case" heading="Maternité" />
+        <CaseDescription>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet t amet, consectetur adipis ing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet. 
+        </CaseDescription>
+        <Link href="#" icon={<ArrowUpRight size={24} />}>
+          Ver mais
+        </Link>
+      </CaseContent>
+    </CasesContainer>
+  )
+}
---
+++ src/pages/home/sections/Cases/styles.ts
@@ -0,0 +1,22 @@
+import styled, { css } from "styled-components";
+
+export const CasesContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-bottom: 7.5rem;
+`
+
+export const CaseContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`
+
+export const CaseDescription = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_500};
+    font-size: ${theme.FONT_SIZE.MD};
+  `}
+`
