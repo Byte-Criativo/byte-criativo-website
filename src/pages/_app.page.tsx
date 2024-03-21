7b1380a5ed7602728b078b0ab3f9d01ffbbf2dfe
Bruna <brunaporato@gmail.com>
2024-03-21 15:54:14 -0300

    feat: seo configs

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,10 +3,19 @@ import StyledComponentsRegistry from "../lib/registry"
 import theme from "../styles/theme"
 import { GlobalStyles } from "../styles/global"
 import { AppProps } from "next/app"
+import { DefaultSeo } from "next-seo"
 
 export default function App({ Component, ...pageProps }: AppProps) {
   return (
     <StyledComponentsRegistry>
+      <DefaultSeo
+        openGraph={{
+          type: "website",
+          locale: "pt_BR",
+          url: "https://www.bytecriativotech.com.br//",
+          siteName: "Byte Criativo",
+        }}
+      />
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <Component {...pageProps} />
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,21 +9,28 @@ import { CTASection } from "./sections/CTA"
 import { FooterSection } from "./sections/Footer"
 import { TeamSection } from "./sections/Team"
 import { FAQSection } from "./sections/FAQ"
+import { NextSeo } from "next-seo"
 
 export default function Home() {
   return (
-    <HomeContainer>
-      <Header />
-      <HomeContent>
-        <HeroSection />
-        <CardsSection />
-        <CasesSection />
-        <ServicesSection />
-        <TeamSection />
-        <FAQSection />
-        <CTASection />
-      </HomeContent>
-      <FooterSection />
-    </HomeContainer>
+    <>
+      <NextSeo
+        title="Byte Criativo | Software House"
+        description="A short description goes here." // TODO: arrumar descrição
+      />
+      <HomeContainer>
+        <Header />
+        <HomeContent>
+          <HeroSection />
+          <CardsSection />
+          <CasesSection />
+          <ServicesSection />
+          <TeamSection />
+          <FAQSection />
+          <CTASection />
+        </HomeContent>
+        <FooterSection />
+      </HomeContainer>
+    </>
   )
 }
